7cb91800753696951bcfc2b123bc5e57ff1f9c5b
shiva arugula <shiva1700903@gmail.com>
2026-03-13 19:55:14 +0530

    Worked on Authentication module

---
+++ TastyBlazor/Utility/SD.cs
@@ -0,0 +1,8 @@
+﻿namespace TastyBlazor.Utility
+{
+    public static class SD
+    {
+        public static string Role_Admin = "Admin";
+        public static string Role_Customer = "Customer";
+    }
+}
